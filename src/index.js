a034678fd95bda3a0eb0ba0636b1f8c6d20862d9
Carson Schwalm <cschwalm@indiana.edu>
2017-09-24 14:27:56 -0400

    adds basic components with first animation on trigger

--- src/index.js
+++ src/index.js
@@ -1,8 +1,93 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import classNames from 'classnames';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Toast extends React.Component {
+  render() {
+    return (
+      <div className="bread">
+      </div>
+    );
+  }
+}
+
+class Trigger extends React.Component {
+ constructor (props) {
+    super(props)
+    this.state = {}
+    this.state.cooking = false
+    this.startCooking = this.startCooking.bind(this)
+  }
+  componentDidMount () {
+    const elm = this.refs.button
+    //elm.addEventListener('animationend', this.resting)
+  }
+  componentWillUnmount () {
+    const elm = this.refs.button
+    //elm.removeEventListener('animationend', this.resting)
+  }
+  startCooking () {
+    this.setState({cooking: true})
+  }
+  render () {
+    const liClasses = classNames({
+      'trigger-btn': true,
+      'cooking': this.state.cooking
+    });	
+		return (
+			<div className="toaster-side">
+				<div className="trigger-track">
+				</div>
+				<button
+	        ref='button'
+        	onClick={() => this.startCooking()}
+        	className={liClasses}>
+				</button>
+			</div>
+		)
+	}
+}	
+
+class Slot extends React.Component {
+	render() {
+		return (
+			<div className="slot centered"></div>
+		)
+	}
+}	
+
+class Toaster extends React.Component {
+  render() {
+    return (
+      <div className="toaster centered">
+      		<Trigger />
+      		<Slot />
+      	<div className="toaster-bottom">
+      	</div>
+      </div>
+    );
+  }
+}
+
+class Kitchen extends React.Component {
+  render() {
+    return (
+      <div className="kitchen">
+        <div className="kitchen-table">
+          <Toaster />
+        </div>
+        <div className="plate">
+          <div></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Kitchen />,
+  document.getElementById('root')
+);
